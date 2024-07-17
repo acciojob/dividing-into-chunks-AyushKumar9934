bf6c7d05eed7993b7c0a2da18c18968c69099b51
AyushKumar9934 <83179865+AyushKumar9934@users.noreply.github.com>
2024-07-17 14:36:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
 
 const divide = (arr, n) => {
-  // Write your code here
-};
+    let finalans=[];
+    let i=0;
+    
+    let tempsum=0;
+    let tempans=[];
+    while(i<arr.length){
+        tempsum+=arr[i];
+        if(tempsum<=n){
+            tempans.push(arr[i]);
+            i++;
+            continue;
+        }
+        else{
+          finalans.push(tempans);
+          tempsum=0;
+          tempans=[];
+        }
 
-const n = prompt("Enter n: ");
+      
+
+
+       
+    }
+    if(tempans.length>0){
+        finalans.push(tempans);
+    }
+    
+    // Write your code here
+return finalans;
+
+      
+  };
+
+ const n = prompt("Enter n: ");
 alert(JSON.stringify(divide(arr, n)));
